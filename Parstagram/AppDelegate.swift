505b6e7f0f6d037df455ba95e4e7cffa8ab74282
Malcolm Johnson <malcolm.johnson423@gmail.com>
2021-03-14 23:48:04 -0400

    created instagram

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "V4O0TUNalySGEcts7oDEeVAgT7NHtQohjFeDwy26" // <- UPDATE
+                    $0.clientKey = "NeSmhB8RA5Kugeuo80JKBkaVsZOXiYpqfco6jNKR" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
         return true
     }
 
